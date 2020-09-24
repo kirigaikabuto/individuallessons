52502bd4cc58c17fd4c0e0f8374f919d24dbfa71
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-09-24 22:19:13 +0600

    sdsd

---
+++ lesson1/3.py
@@ -0,0 +1,8 @@
+a = "hello"
+b = "world"
+c = a + " " + b#str
+d = 6 #int
+#TypeError: can only concatenate str (not "int") to str
+e = c + 6
+l = 6.5 + 12 #
+print(e)
---
+++ lesson1/4.py
@@ -0,0 +1,8 @@
+# a = "hello"
+# # d = a * 3
+# # print(d)
+a = 3
+a = 6
+a = 19
+b = 30
+print(a)
